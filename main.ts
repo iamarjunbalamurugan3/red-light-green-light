192287617e4991df5c793e1563f7ca1e05789c39
iamarjunbalamurugan3 <113736647+iamarjunbalamurugan3@users.noreply.github.com>
2023-04-13 18:45:30 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,21 +2,24 @@ input.onButtonPressed(Button.A, function () {
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
     basic.pause(100)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
-    music.playMelody("G G G G G G G G ", 500)
-    music.playMelody("G G G G G G G G ", 500)
+    music.playTone(262, music.beat(BeatFraction.Double))
+})
+input.onButtonPressed(Button.AB, function () {
+    while (true) {
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(randint(1000, 3500))
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(randint(500, 1000))
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(randint(500, 2000))
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
     basic.pause(100)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-    music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 500)
-    music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 500)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
-    music.playMelody("B A G A G F A C5 ", 159)
-})
-basic.forever(function () {
-    music.setVolume(255)
+    music.playTone(523, music.beat(BeatFraction.Eighth))
 })
